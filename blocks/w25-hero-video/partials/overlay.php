c569440fb2f64d9b9c44c543231da82fab3345d5
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-01-08 16:07:57 +0100

    credits for video and swiper

--- blocks/w25-hero-video/partials/overlay.php
+++ blocks/w25-hero-video/partials/overlay.php
@@ -1,5 +1,5 @@
-<div class="overlay absolute top-0 left-0 z-20 w-full h-full">
-  <div class="hidden md:block overlayPuzzles absolute inset-0">
+<div class="absolute top-0 left-0 z-20 w-full h-full overlay">
+  <div class="absolute inset-0 hidden md:block overlayPuzzles">
     <?php for ($i = 1; $i <= 6; $i++): ?>
       <img
         class="overlayPuzzle desktop object-cover w-full h-full absolute inset-0 <?php echo $i === 1 ? 'opacity-100' : 'opacity-0'; ?>"
@@ -10,7 +10,7 @@
     <?php endfor; ?>
   </div>
 
-  <div class=" md:hidden overlayPuzzles absolute inset-0">
+  <div class="absolute inset-0 md:hidden overlayPuzzles">
     <?php for ($i = 1; $i <= 4; $i++): ?>
       <img
         class="overlayPuzzle mobile object-cover w-full h-full absolute inset-0 <?php echo $i === 1 ? 'opacity-100' : 'opacity-0'; ?>"
@@ -28,4 +28,22 @@
   >
 
   <?php get_template_part('blocks/w25-hero-video/partials/play-button'); ?>
+  <?php if (have_rows('video_credits')): ?>
+  <div class="absolute left-0 px-2 bottom-2" x-data="{ showCredits: false }">
+    <div x-show="showCredits" class="max-w-xl p-2 mb-2 space-y-2 text-sm text-white bg-black rounded-lg">
+      <?php while (have_rows('video_credits')): the_row();
+        $credit = get_sub_field('credit');
+        if ($credit):
+      ?>
+        <p><?php echo $credit; ?></p>
+      <?php
+        endif;
+      endwhile; ?>
+    </div>
+    <button @click="showCredits = !showCredits" class="flex items-center gap-1 p-2 text-white transition rounded-lg hover:bg-black/70">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11 17h2v-6h-2zm1-8q.425 0 .713-.288T13 8t-.288-.712T12 7t-.712.288T11 8t.288.713T12 9m0 13q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"/></svg>
+    <span>Credits</span>
+    </button>
+  </div>
+  <?php endif; ?>
 </div>
