6606f37360fffcfdc2e147808699da6654dd7863
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-01-12 09:55:49 +0100

    removed bbWikimediaCommonsMedia

--- template-parts/featured-image-rounded.php
+++ template-parts/featured-image-rounded.php
@@ -2,7 +2,7 @@
     <div class="bb-block-image aspect-w-16 aspect-h-9 bg-neutral-light rounded-3xl">
         <figure class="w-full w-full">
             <?php the_post_thumbnail('large', ['class' => 'rounded-3xl object-cover w-full h-full overflow-hidden']); ?>
-            <?php if (bbWikimediaCommonsMedia::has_post_thumbnail_caption()): ?>
+            <?php if (get_the_post_thumbnail_caption()): ?>
             <figcaption class="invisible nohover:hidden flex absolute rounded-b-3xl
   left-0 bottom-0 right-0 text-white bg-neutral-dark w-auto h-auto z-20 p-2 text-sm flex items-start gap-4 break-all">
                 <?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?></div>
@@ -10,7 +10,7 @@
             <?php endif; ?>
         </figure>
     </div>
-    <?php if (bbWikimediaCommonsMedia::has_post_thumbnail_caption()): ?>
+    <?php if (get_the_post_thumbnail_caption()): ?>
     <figcaption class="hidden nohover:flex rounded-3xl  left-0 bottom-0 right-0 text-white bg-neutral-dark w-auto h-auto z-20 p-2 text-sm flex items-start gap-4 break-all">
         <?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?></div>
     </figcaption>
