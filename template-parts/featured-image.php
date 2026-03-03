7c674d6c02008effb443c7883d35432109eb9dd1
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-03-03 10:44:56 +0100

    caption visibility

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -1,20 +1,18 @@
+<?php $has_caption = get_the_post_thumbnail_caption(); ?>
 <div class="relative">
-    <div class="bb-block-image bg-neutral-light aspect-w-16 aspect-h-9 rounded-t-3xl">
-        <figure class="w-full">
-            <?php the_post_thumbnail('large', ['class' => 'rounded-t-3xl object-cover w-full h-full overflow-hidden']); ?>
-            <?php if (get_the_post_thumbnail_caption()): ?>
-            <figcaption
-                class="absolute bottom-0 left-0 right-0 z-20 flex items-start invisible w-auto h-auto gap-4 p-2 text-sm text-white break-all nohover:hidden bg-black/80">
-                <?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?>
-                </div>
-            </figcaption>
-            <?php endif; ?>
-        </figure>
-    </div>
-    <?php if (get_the_post_thumbnail_caption()): ?>
-    <figcaption
-        class="bottom-0 left-0 right-0 z-20 flex items-start hidden w-auto h-auto gap-4 p-2 text-sm text-white break-all nohover:flex rounded-3xl bg-black/80">
-        <?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?></div>
-    </figcaption>
-    <?php endif; ?>
+<div class="bg-gray-200 bb-image-block aspect-w-16 aspect-h-9 rounded-t-3xl">
+  <figure class="w-full">
+  <?php the_post_thumbnail('large', ['class' => 'rounded-t-3xl object-cover w-full h-full overflow-hidden']); ?>
+  <?php if ($has_caption): ?>
+  <figcaption class="absolute bottom-0 left-0 right-0 z-20 flex items-start invisible w-auto h-auto gap-4 p-2 text-sm text-white break-all bg-gray-900 nohover:hidden">
+    <?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?></div>
+  </figcaption>
+  <?php endif; ?>
+  </figure>
+</div>
+<?php if ($has_caption): ?>
+<figcaption class="bottom-0 left-0 right-0 z-20 flex items-start hidden w-auto h-auto gap-4 p-2 mb-4 text-sm text-white break-all bg-gray-900 rounded-b-lg nohover:flex md:rounded-2xl md:mb-0">
+  <?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?></div>
+</figcaption>
+<?php endif; ?>
 </div>
