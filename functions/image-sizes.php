dc962c895bb6e534ba60ce047dbd03bbc85b30e0
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-04-23 14:15:22 +0200

    added image sizes from wmde to fix video player protrait

--- functions/image-sizes.php
+++ functions/image-sizes.php
@@ -1,4 +1,5 @@
 <?php
+$sqrt2_ratio = 1.4141;
 $grid_container = 1440;
 $twelve_columns = $grid_container;
 $eight_columns = ($grid_container / 12) * 8;
@@ -6,73 +7,21 @@ $six_columns = ($grid_container / 12) * 6;
 $four_columns = ($grid_container / 12) * 4;
 $three_columns = ($grid_container / 12) * 3;
 $two_columns = ($grid_container / 12) * 2;
-// 4:3
-$twelve_columns_four_three_short_side = ($twelve_columns / 4) * 3;
-$eight_columns_four_three_short_side = ($eight_columns / 4) * 3;
-$six_columns_four_three_short_side = ($six_columns / 4) * 3;
-$four_columns_four_three_short_side = ($four_columns / 4) * 3;
-$three_columns_four_three_short_side = ($three_columns / 4) * 3;
-$two_columns_four_three_short_side = ($three_columns / 4) * 3;
-// 16:9
-$twelve_columns_sixteen_nine_short_side = ($twelve_columns / 16) * 9;
-$eight_columns_sixteen_nine_short_side = ($eight_columns / 16) * 9;
-$six_columns_sixteen_nine_short_side = ($six_columns / 16) * 9;
-$four_columns_sixteen_nine_short_side = ($four_columns / 16) * 9;
-$three_columns_sixteen_nine_short_side = ($three_columns / 16) * 9;
-$two_columns_sixteen_nine_short_side = ($three_columns / 16) * 9;
-// 9:16
-$twelve_columns_nine_sixteen_long_side = ($twelve_columns / 9) * 16;
-$eight_columns_nine_sixteen_long_side = ($eight_columns / 9) * 16;
-$six_columns_nine_sixteen_long_side = ($six_columns / 9) * 16;
-$four_columns_nine_sixteen_long_side = ($four_columns / 9) * 16;
-$three_columns_nine_sixteen_long_side = ($three_columns / 9) * 16;
-$two_columns_nine_sixteen_long_side = ($two_columns / 9) * 16;
+// 16:9 Landscape - height = width * (9/16)
+$twelve_columns_landscape_height = round($twelve_columns * (9 / 16));
+$six_columns_landscape_height = round($six_columns * (9 / 16));
+$three_columns_landscape_height = round($three_columns * (9 / 16));
+$two_columns_landscape_height = round($two_columns * (9 / 16));
+// 9:16 Portrait - height = width * (16/9)
+$twelve_columns_portrait_height = round($twelve_columns * (16 / 9));
+$six_columns_portrait_height = round($six_columns * (16 / 9));
+$three_columns_portrait_height = round($three_columns * (16 / 9));
+$two_columns_portrait_height = round($two_columns * (16 / 9));
 
 /* Image Size Crop for foundation */
-add_image_size('twelve-columns', $twelve_columns, 0, false);
-// add_image_size('eight-columns', $eight_columns, 0, false);
-// add_image_size('six-columns', $six_columns, 0, false);
-add_image_size('four-columns', $four_columns, 0, false);
-// add_image_size('three-columns', $three_columns, 0, false);
-add_image_size('two-columns', $two_columns, 0, false);
-/* 4:3 Crop */
-add_image_size('twelve-columns-four-three', $twelve_columns, $twelve_columns_four_three_short_side, true);
-// add_image_size('eight-columns-four-three', $eight_columns, $eight_columns_four_three_short_side, true);
-// add_image_size('six-columns-four-three', $six_columns, $six_columns_four_three_short_side, true);
-add_image_size('four-columns-four-three', $four_columns, $four_columns_four_three_short_side, true);
-// add_image_size('three-columns-four-three', $three_columns, $three_columns_four_three_short_side, true);
-add_image_size('two-columns-four-three', $two_columns, $two_columns_four_three_short_side, true);
-/* 3:4 Crop */
-add_image_size( 'six-columns-three-four', 600, 800, true );
-/* 16:9 Crop */
-add_image_size('twelve-columns-sixteen-nine', $twelve_columns, $twelve_columns_sixteen_nine_short_side, true);
-// add_image_size('eight-columns-sixteen-nine', $eight_columns, $eight_columns_sixteen_nine_short_side, true);
-// add_image_size('six-columns-sixteen-nine', $six_columns, $six_columns_sixteen_nine_short_side, true);
-add_image_size('four-columns-sixteen-nine', $four_columns, $four_columns_sixteen_nine_short_side, true);
-// add_image_size('three-columns-sixteen-nine', $three_columns, $three_columns_sixteen_nine_short_side, true);
-add_image_size('two-columns-sixteen-nine', $two_columns, $two_columns_sixteen_nine_short_side, true);
-/* 9:16 Crop */
-add_image_size('twelve-columns-nine-sixteen', $twelve_columns, $twelve_columns_nine_sixteen_long_side, true);
-// add_image_size('eight-columns-nine-sixteen', $eight_columns, $eight_columns_nine_sixteen_long_side, true);
-// add_image_size('six-columns-nine-sixteen', $six_columns, $six_columns_nine_sixteen_long_side, true);
-add_image_size('four-columns-nine-sixteen', $four_columns, $four_columns_nine_sixteen_long_side, true);
-// add_image_size('three-columns-nine-sixteen', $three_columns, $three_columns_nine_sixteen_long_side, true);
-add_image_size('two-columns-nine-sixteen', $two_columns, $two_columns_nine_sixteen_long_side, true);
-
-/* Minimum Upload Sizes */
-//add_filter('wp_handle_upload_prefilter','tc_handle_upload_prefilter');
-function tc_handle_upload_prefilter($file)
-{
-	$img = getimagesize($file['tmp_name']);
-	$minimum = ['width' => '800', 'height' => '600'];
-	$width = $img[0];
-	$height = $img[1];
-
-	if ($width < $minimum['width']) {
-		return ['error' => "Image dimensions are too small. Minimum width is {$minimum['width']}px. Uploaded image width is $width px"];
-	} elseif ($height < $minimum['height']) {
-		return ['error' => "Image dimensions are too small. Minimum height is {$minimum['height']}px. Uploaded image height is $height px"];
-	} else {
-		return $file;
-	}
-}
+/* 16:9 Landscape Crop */
+add_image_size('six-columns-landscape', $six_columns, $six_columns_landscape_height, true);
+add_image_size('three-columns-landscape', $three_columns, $three_columns_landscape_height, true);
+/* 9:16 Portrait Crop */
+add_image_size('six-columns-portrait', $six_columns, $six_columns_portrait_height, true);
+add_image_size('three-columns-portrait', $three_columns, $three_columns_portrait_height, true);
